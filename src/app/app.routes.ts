810ce8a1d08b42a202a8534e4478cda635990db6
scott <daniel.scott.aguilar.aguirre@gmail.com>
2026-05-08 11:34:14 -0400

    enrute pag inicio,login y reserva

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './pages/inicio/inicio';
+import { Login } from './pages/login/login';
+import { Reserva } from './pages/reserva/reserva';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+{ path: '', component:Inicio},
+{ path: 'login', component:Login},
+{ path: 'reserva', component:Reserva}
+];
