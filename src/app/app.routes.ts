44132fc53275d5b343e32f170c3ab4327b7876fc
scott <daniel.scott.aguilar.aguirre@gmail.com>
2026-05-08 20:34:11 -0400

    se agruega login funcional junto a nuevas carpetas de registro y se completa la funcionalidad del registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Inicio } from './pages/inicio/inicio';
 import { Login } from './pages/login/login';
 import { Reserva } from './pages/reserva/reserva';
+import { Registro } from './pages/registro/registro';
 
 
 export const routes: Routes = [
 { path: '', component:Inicio},
 { path: 'login', component:Login},
-{ path: 'reserva', component:Reserva}
+{ path: 'reserva', component:Reserva},
+{ path: 'registro', component:Registro}
 ];
