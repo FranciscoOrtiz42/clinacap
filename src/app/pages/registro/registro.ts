820d67daabebcc896f30b81ae50ee78e94c5064d
scott <daniel.scott.aguilar.aguirre@gmail.com>
2026-05-09 23:54:32 -0400

    respaldo registro y login

--- src/app/pages/registro/registro.ts
+++ src/app/pages/registro/registro.ts
@@ -1,31 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule, NgIf, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,FormsModule,CommonModule,NgIf,NgClass],
   templateUrl: './registro.html',
   styleUrls: ['./registro.css'],
 })
-export class Registro {// Variables para vincular con el HTML
+export class Registro {
   nombre: string = '';
-  fechaNac: string = '';
+  correo: string = '';
   password: string = '';
 
+  // 2. Inyecta el servicio en el constructor
+  constructor(private router: Router) { }
+
   guardarTemporalmente() {
-    // Creamos un objeto con los datos
     const usuarioData = {
       nombre: this.nombre,
-      fecha: this.fechaNac,
+      correo: this.correo,
       pass: this.password
     };
 
-    // Lo guardamos en el sessionStorage (convertido a texto)
+    // Guardamos los datos
     sessionStorage.setItem('usuarioLogueado', JSON.stringify(usuarioData));
+    
+    console.log('Datos guardados con éxito');
 
-    console.log('Datos guardados en sesión:', usuarioData);
-    alert('¡Datos guardados temporalmente con éxito!');
+    // 3. Redirige al usuario al login
+    this.router.navigate(['/login']); 
   }
 }
 //esta esta aun sin terminar se requiere agregar funcionalidad en registro.html
